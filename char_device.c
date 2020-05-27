d6fe026fd2d92a0e7e9f6a307065d1ae176f8675
janardhannarayana <janardhannarayana@gmail.com>
2020-05-27 22:58:06 +0530

    Multiple device nodes support:
    Added multiple device nodes support

--- char_device.c
+++ char_device.c
@@ -13,26 +13,58 @@
 #include <pthread.h>
 #include <fcntl.h>
 #include <sys/sysmacros.h>
+#include <sys/wait.h>
 
+
+void *pthread_func1(void *major)
+{
+    dev_t dev;
+    
+    dev = makedev(*(int *)major, 0);
+    mknod("./chardevice1", S_IFCHR, dev);
+    
+    int fd = open("./chardevice1", O_RDWR);
+    printf("func1: tid = %lu, fd = %d\n",pthread_self(), fd);
+    char buff[128];
+//`    write(fd, "THIS IS A SAMPLE CHARACTER DEVICE 1\n", sizeof("THIS IS A SAMPLE CHARACTER DEVICE 1\n"));
+    sleep(3);
+    read(fd, buff, sizeof("THIS IS A SAMPLE CHARACTER DEVICE 1\n"));
+    printf("tid = %lu, %s\n", pthread_self(), buff);
+
+    return NULL;
+}
+
+void *pthread_func2(void *major)
+{
+    dev_t dev;
+    dev = makedev(*(int *)major, 1);
+    mknod("./chardevice2", S_IFCHR, dev);
+    
+    int fd = open("./chardevice2", O_RDWR);
+    printf("func2: tid = %lu, fd = %d\n", pthread_self(), fd);
+    char buff[128];
+    write(fd, "THIS IS A SAMPLE CHARACTER DEVICE 2\n", sizeof("THIS IS A SAMPLE CHARACTER DEVICE 2\n"));
+    read(fd, buff, sizeof("THIS IS A SAMPLE CHARACTER DEVICE 2\n"));
+    printf("tid = %lu, %s\n", pthread_self(), buff);
+
+    return NULL;
+}
 
 int main(int argc, char *argv[])
 {
     if (argc < 2) {
 	printf("Usage: executable <major number>\n");
 	return 0;
-    }	
+    }
 
     dev_t dev = 0;
     int major = atoi(argv[1]);
+    pthread_t pid1;
+    pthread_t pid2;
 
-    dev = makedev(major, 0);
-    mknod("./chardevice1", S_IFCHR, dev);
-
-    int fd = open("./chardevice1", O_RDWR);
-    printf("fd = %d\n", fd);
-    char buff[128];
-    write(fd, "THIS IS A SAMPLE CHARACTER DEVICE DRIVER\n", sizeof("THIS IS A SAMPLE CHARACTER DEVICE DRIVER\n"));
-    read(fd, buff, sizeof("THIS IS A SAMPLE CHARACTER DEVICE DRIVER\n"));
-    printf("%s\n", buff);
+    pthread_create(&pid1, NULL, pthread_func1, &major);
+    pthread_create(&pid2, NULL, pthread_func2, &major);
+    pthread_join(pid1, NULL);
+    pthread_join(pid2, NULL);
     return 0;
 }
--- char_driver.c
+++ char_driver.c
@@ -19,18 +19,18 @@
  * permission for sysfs.
  * Ex:insmod char_driver.ko "hello_print=4"
  * charp is the type for char to pointer
-*/
+ */
 
-static int hello_print = 1;
-module_param(hello_print, int, S_IRUGO);
-MODULE_PARM_DESC(hello_print, "This parameter prints the hello world");
+static int num_dev = 1;
+module_param(num_dev, int, S_IRUGO);
+MODULE_PARM_DESC(num_dev, "This parameter specifies the number of device nodes this driver support");
 
 struct mydevice {
     struct cdev dev_cdev;
     char *data;
 };
 
-struct mydevice *chardevices;
+struct mydevice *chardevices = NULL;
 dev_t dev;
 
 int dev_open(struct inode *dev_inode, struct file *dev_file);
@@ -47,27 +47,31 @@ struct file_operations dev_fops = {
 int dev_open(struct inode *dev_inode, struct file *dev_file)
 {
     printk(KERN_ALERT "Open function\n");
+    dev_file->private_data = container_of(dev_inode->i_cdev, struct mydevice, dev_cdev);
+    printk("private data = %p\n", dev_file->private_data);
     return 0;
 }
 
 /* Specifying variable as __user indicated the kernel that the data is in user space
-*  This can avoid serious mistakes by some softwares like coverity.
-*  Read reads the kernel data and copy into the user buffer.
-*  Write writes the user buffer data into kernel
-*  Hence const used in write call.
-*/
+ *  This can avoid serious mistakes by some softwares like coverity.
+ *  Read reads the kernel data and copy into the user buffer.
+ *  Write writes the user buffer data into kernel
+ *  Hence const used in write call.
+ */
 
 ssize_t dev_read(struct file *fp, char __user *userbuff, size_t size, loff_t *offset)
 {
+    struct mydevice *device = fp->private_data;
     printk(KERN_ALERT "Read function\n");
-    copy_to_user(userbuff, chardevices->data, size);
+    copy_to_user(userbuff, device->data, size);
     return 0;
 }
 
 ssize_t dev_write(struct file *fp, const char __user *userbuff, size_t size, loff_t *offset)
 {
+    struct mydevice *device = fp->private_data;
     printk(KERN_ALERT "Write function\n");
-    copy_from_user(chardevices->data, userbuff, size);
+    copy_from_user(device->data, userbuff, size);
     printk(KERN_ALERT "Data from user = %s", chardevices->data);
     return 0;
 }
@@ -78,66 +82,71 @@ ssize_t dev_write(struct file *fp, const char __user *userbuff, size_t size, lof
  * Specifying __init to the function will let the kernel know that the function is used only
  * for initialization. Hence once the module is loaded kernel remove the space used for init function.
  * This is applicable only for built-in-drivers. For loadable modules this will get ignored.
-*/
+ */
 static int __init char_module_init(void)
 {
-//    dev_t dev;
-    while(hello_print) {
-	printk(KERN_ALERT "Hello World!!!\n");
-	hello_print--;
-    }
+    int i = 0;
+
+    printk(KERN_ALERT "Sample charachter driver initialization\n");
 
     /* Major number and minor number are used to identify the device driver and the device
-    * associated to that driver.
-    * We can create multiple device with same major number and minor number. It is user responsibility
-    * to take care of that.
-    * alloc_chrdev_region just registers the driver with the kernel, but it wont allocate the cdev structure.
-    * Due to this you can still see the device in /proc/devices and create a device too.
-    * A driver needs to register with cdev structure to access all the operations.
-    * second argument in alloc_chrdev_region denotes the base minor number.
-    * third argument in alloc_chrdev_region denotes the number of devices you can create.
-    * You can still create the device nodes with less that base number or more than the number of
-    * devices you specified.
-    * Ex: (alloc_chrdev_region(&dev, 2, 6, "CHAR DEVICE")
-    * This can create a total of 6 devices with device number starting from 2
-    * However user can still create 7 devices or device minor number starting with 0. But the kernel
-    * does not operate function operations like open, User always get -1 as FD
-    */
-    
-    if (alloc_chrdev_region(&dev, 0, 1, "CHAR DEVICE") != 0) {
+     * associated to that driver.
+     * We can create multiple device with same major number and minor number. It is user responsibility
+     * to take care of that.
+     * alloc_chrdev_region just registers the driver with the kernel, but it wont allocate the cdev structure.
+     * Due to this you can still see the device in /proc/devices and create a device too.
+     * A driver needs to register with cdev structure to access all the operations.
+     * second argument in alloc_chrdev_region denotes the base minor number.
+     * third argument in alloc_chrdev_region denotes the number of devices you can create.
+     * You can still create the device nodes with less that base number or more than the number of
+     * devices you specified.
+     * Ex: (alloc_chrdev_region(&dev, 2, 6, "CHAR DEVICE")
+     * This can create a total of 6 devices with device number starting from 2
+     * However user can still create 7 devices or device minor number starting with 0. But the kernel
+     * does not operate function operations like open, User always get -1 as FD
+     */
+
+    if (alloc_chrdev_region(&dev, 0, num_dev, "CHAR DEVICE") != 0) {
 	printk(KERN_ALERT  "Char device registration failed\n");
 	return -1;
     } else {
 	printk(KERN_ALERT "Device major number %d minor number %d\n", MAJOR(dev), MINOR(dev));
     }
 
-    chardevices = kmalloc(sizeof(struct mydevice), GFP_KERNEL);
-    chardevices->data = kmalloc(128, GFP_KERNEL);
-    cdev_init(&(chardevices->dev_cdev), &dev_fops);
+    chardevices = kmalloc(sizeof(struct mydevice) * num_dev, GFP_KERNEL);
 
-    if (cdev_add(&(chardevices->dev_cdev), dev, 1) != 0 ) {
-	printk(KERN_ALERT "Fail to add the character device\n");
-	kfree(chardevices->data);
-	kfree(chardevices);
-	unregister_chrdev_region(0, 1);
-	return -1;
+    /* Based on number device nodes creating, that many cdev should be added with different
+    * minor numbers. Device nodes which creates using this particular major and minor number
+    * will have the different cdev structures allocated*/
+    for(i = 0; i < num_dev; i++) {
+	int mydev = MKDEV(MAJOR(dev), i);
+	(chardevices + i)->data = kmalloc(128, GFP_KERNEL);
+	memcpy((chardevices + i)->data, "Default string\n", 15);
+	(chardevices + i)->data[15] = '\0';
+	cdev_init(&((chardevices + i)->dev_cdev), &dev_fops);
+	cdev_add(&((chardevices + i)->dev_cdev), mydev, 1);
     }
-    
+
     return 0;
 }
 
 static void __exit char_module_exit(void)
 {
-    cdev_del(&(chardevices->dev_cdev));
-    kfree(chardevices->data);
-    kfree(chardevices);
-    unregister_chrdev_region(dev, 1);
-    
+    int i = 0;
+
+    for(i = 0; i < num_dev; i++) {
+	cdev_del(&((chardevices + i)->dev_cdev));
+	kfree((chardevices + i)->data);
+	kfree(chardevices + i);
+    }
+    unregister_chrdev_region(dev, num_dev);
+
     printk(KERN_ALERT"BYE!!! BYE!!!\n");
 }
 module_init(char_module_init);
 module_exit(char_module_exit);
 
+/*Without license when we load the driver in kernel, kernel shows a tainted kernel message*/
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("JANARDHAN");
 MODULE_DESCRIPTION("This is a sample characer device driver program");
